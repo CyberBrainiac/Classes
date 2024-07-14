8b7b53ddab9a48ee367214db8916c6b951fa9cf4
Arseniy Arkhipov <arsenij.arxipov.1998@gmail.com>
2024-07-14 15:53:52 +0300

    update babel and eslint

--- src/chair.js
+++ src/chair.js
@@ -1,46 +1,36 @@
 export class Chair {
+  static backSupport = true;
+  static armRests = true;
+
   constructor(
     color,
     seatHeight,
     recliningAngle,
-    backSupport = true,
     headSupport = true,
     padding = '3 inch',
-    armRests = true,
     seatSize = '16 inch',
     isHeightAdjustable = false,
-    isMovable = false,
+    isMovable = false
   ) {
     this.type = 'Chair';
     this.color = color;
     this.seatHeight = seatHeight;
     this.recliningAngle = recliningAngle;
-    this.backSupport = backSupport;
     this.headSupport = headSupport;
     this.padding = padding;
-    this.armRests = armRests;
     this.seatSize = seatSize;
     this.isHeightAdjustable = isHeightAdjustable;
     this.isMovable = isMovable;
+  }
+
+  static logObjectProps() {
+    console.dir(this);
   }
 
   adjustableHeight() {}
   adjustAngle() {}
   moveChair() {}
 }
-
-const newChair = new Chair(
-  'Blue',
-  '25 inch',
-  '20 deg',
-  true,
-  false,
-  '3 inch',
-  true,
-  '16 inch',
-  false,
-  false,
-);
 
 // console.dir(Chair);
 // console.log('Chair Object', newChair);
--- src/superForObject.js
+++ src/superForObject.js
@@ -1,18 +1,19 @@
 export const parent = {
-  name: parent,
+  name: 'parent',
   showName() {
     return this.name;
   },
 };
 
 const child = {
-  name: child,
+  name: 'child',
   askMoney() {
-    const investor = super.name;
+    const investor = super.showName.call(parent);
     return `give me money ${investor}`;
   },
 };
 
 Object.setPrototypeOf(child, parent);
 
-console.log(child);
+// console.log(child);
+// console.log(child.askMoney());
